e7fc76fa358b748f7eb66536168fe5d418e324de
Lloyd <lloydmeta@gmail.com>
2017-07-01 01:45:26 +0900

    Add doctest

--- src/main/g8/src/main/scala/$organisation__packaged$/Main.scala
+++ src/main/g8/src/main/scala/$organisation__packaged$/Main.scala
@@ -4,4 +4,16 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello World!!!")
   }
+
+  /**
+   * This is how to write a doctest
+   *
+   * # Example
+   *
+   * {{{
+   * scala> Main.doctested(true)
+   * res0: Boolean = false
+   * }}}
+   */
+  def doctested(b: Boolean): Boolean = !b
 }
